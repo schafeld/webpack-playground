c5715a21d8b5e72d31ac909c7e1943583d0ca2dd
Oliver Schafeld <info@schafeld.com>
2019-01-19 13:59:19 +0100

    add comment regarding buggy VS Code editor extension

---
+++ config/webpack.dev.js
@@ -0,0 +1,9 @@
+// Some extensions  @disabled
+// Silly [ts] error hints disappered after looking at extensions filtered by @builtin javascript 
+// 'TS and JS Language Features' disabled for workspace.
+// Also disabled for workspace: JSLint extension
+module.exports {
+    entry: {},
+    mode: '',
+    output: {}
+}
