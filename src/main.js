f46de28f5ca29414e369cb8d18613e0be6b0fce3
Oliver Schafeld <info@schafeld.com>
2019-01-22 20:58:58 +0100

    process html and images with webpack too

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 // @builtin javascript | JS and TS extension disabled to avoid false TS errors
 require('./main.css')
+require('./index.html')
 
-console.log('test')
+// console.log('test')
 
 const app = document.getElementById('app')
 app.innerHTML = '<p class="centerbox">This was replaced by JS, yay!</p>'
